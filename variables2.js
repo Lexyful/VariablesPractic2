3fe191d968dcb2c5ab75a943a950800f33ade69d
Alexys Jordan <alexyskjordan@gmail.com>
2022-11-10 08:23:18 -0700

    Add Initialcommit

---
+++ variables2.js
@@ -0,0 +1,26 @@
+var email = "alexyskjordan@gmail.com"
+console.log(email)
+
+var name = "Alexys"
+console.log (name)
+
+var nickName = "Lexye"
+console.log(nickName)
+
+var fakSocial = 456789011
+console.log(fakSocial)
+
+var fakePhone =7779994444
+console.log(fakePhone)
+
+var age = 30
+console.log(age)
+
+var lovesFamily = true
+console.log(lovesFamily)
+
+var hatesReading = false
+console.log(hatesReading)
+
+var eatsBananas = true
+console.log(eatsBananas)
